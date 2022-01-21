01983e21b7ff167aaeb19ba06a1d6b9715456351
alu0101205953 <alu0101205953@ull.edu.es>
2022-01-21 11:19:03 +0000

    Test correction

--- happyharvest/test/Components.test.js
+++ happyharvest/test/Components.test.js
@@ -174,7 +174,7 @@ describe("Testing <Homepage/> Component", () => {
    });
    it("Should render main page if user is logged", () => {
       const wrapper = shallow(<Homepage props={{username: "test"}}/>);
-      expect(wrapper.containsMatchingElement(<h1>Happy Harvest</h1>)).to.be.false;
+      expect(wrapper.containsMatchingElement(<h1>Happy Harvest</h1>)).to.be.true;
    });
 
    chai.use(chaiEnzyme());
